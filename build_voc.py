ff35f96b3813ebad35a9a61fa4ddcfdd966850e8
YongBeomKim <saltman21@naver.com>
2018-09-11 21:28:48 +0900

    scrept rerange

--- build_voc.py
+++ build_voc.py
@@ -1,14 +1,15 @@
-from konlpy.tag import Twitter
+from konlpy.tag import Okt
 import tensorflow as tf
 import numpy as np
 import config
 from  Conversation import Conversation
 
-#twitter=Twitter() twitter 사용시.
+# --twitter 사용시 이 함수 주석을 전부 풀어주고 아래는 주석처리
+#twitter = Okt()
 #def Make_voc(data_path,voc_path):
-#    words=[]
+#    words = []
 #    with open(data_path, 'r', encoding='utf-8') as content_file:
-#        for con in content_file: 
+#        for con in content_file:
 #            content = content_file.read()
 #            words.extend(twitter.morphs(content))
 #        words = list(set(words))
@@ -16,26 +17,29 @@ from  Conversation import Conversation
 #    with open(voc_path, 'w') as vocab_file:
 #        for w in words:
 #            vocab_file.write(w + '\n')
-#
-# --twitter 사용시 이 함수 주석을 전부 풀어주고 아래의 함수를 주석처리.
 
-def Make_voc(conv,data_path,voc_path):
-    words=[]
 
-    with open(data_path,'r') as f:
-        lines=f.read()
-        words=conv.Tokenizer(lines)
-        words=list(set(words))
+# 대화문에서 Punctuation 을 제외한 단어 Token을 추출한다
+# 대화문에 사용할 one hot encoding 의 Token을 생성한다
+def Make_voc(conv, data_path, voc_path):
+    words = []
+    with open(data_path, 'r') as f:
+        lines = f.read()
+        words = conv.Tokenizer(lines)
+        words = list(set(words))
 
     with open(voc_path,'w') as wf:
          for w in words:
-            wf.write(w+"\n")
-    print("voc complete")
-    
+            wf.write(w + "\n")
+    print("vocabluary's Expliction complete")
+
+
 def main():
-    conv=Conversation()
+    conv = Conversation()
     Make_voc(conv,config.DATA_PATH,config.VOC_PATH)
-#    Make_voc(config.DATA_PATH, config.VOC_PATH) <-twitter 사용시 위를 주석처리하고 이 라인의 주석을 풀어줌.
+
+    # Twitter 모듈 사용시 위를 주석후 이를 활성화
+    # Make_voc(config.DATA_PATH, config.VOC_PATH)
 
 
 if __name__=="__main__":
